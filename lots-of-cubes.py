cc8d946eaa27d1b508c32f55bc713959b922db41
JamesHagerman <james.hagerman@gmail.com>
2015-11-06 02:14:49 -0800

    Updating the API again so it might better support URLs and text based data.

--- lots-of-cubes.py
+++ lots-of-cubes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 print "We are going to send a vec3 so you can draw a cube..."
-print "\x1b[1;0.0;0.0;0.0V"*100
+print "\x1bV1;0.0;0.0;0.0|"*100
 print "Done."
--- multiple-cubes.py
+++ multiple-cubes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 print "We are going to send a vec3 so you can draw a cube..."
-print "\x1b[1;0.0;0.0;0.0V"
-print "\x1b[1;0.0;1.0;1.0V"
-print "\x1b[1;1.0;0.0;1.0V"
+print "\x1bV1;0.0;0.0;0.0|"
+print "\x1bV1;0.0;1.0;1.0|"
+print "\x1bV1;1.0;0.0;1.0|"
 print "Done."
