43f6467e6fb1d642b67089f768c1b784f9b889ec
James Hagerman from pile <james.hagerman@gmail.com>
2015-11-06 02:31:45 -0800

    Updating the scripts to be a little more organized.

--- multiple-cubes.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-print "We are going to send a vec3 so you can draw a cube..."
-print "\x1bV1;0.0;0.0;0.0|"
-print "\x1bV1;0.0;1.0;1.0|"
-print "\x1bV1;1.0;0.0;1.0|"
-print "Done."
---
+++ obj.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+print "We are going to tell PacketVR to load an OBJ from a url we define:"
+print "\x1bV2;0.0;0.0;1.0;http://www.everyday3d/unity3d/obj/monkey.objderp|"
+print "Done."
+
